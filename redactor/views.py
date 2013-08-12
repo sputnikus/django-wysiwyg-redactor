b8886ec42a81cf2be61df3fb1e2844e5e14a259d
Martin Putniorz <mputniorz@gmail.com>
2013-08-12 07:48:21 +0200

    New upload handler

--- redactor/views.py
+++ redactor/views.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
@@ -7,8 +8,6 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 
-from redactor.forms import ImageForm
-
 
 UPLOAD_PATH = getattr(settings, 'REDACTOR_UPLOAD', 'redactor/')
 
@@ -16,13 +15,10 @@ UPLOAD_PATH = getattr(settings, 'REDACTOR_UPLOAD', 'redactor/')
 @csrf_exempt
 @require_POST
 @login_required
-def redactor_upload(request, upload_to=None, form_class=ImageForm, response=lambda name, url: url):
-    form = form_class(request.POST, request.FILES)
-    if form.is_valid():
-        file_ = form.cleaned_data['file']
-        path = os.path.join(upload_to or UPLOAD_PATH, file_.name)
-        real_path = default_storage.save(path, file_)
-        return HttpResponse(
-            response(file_.name, os.path.join(settings.MEDIA_URL, real_path))
-        )
-    return HttpResponse(status=403)
+def redactor_upload(request, upload_to=None):
+    # images = []
+    for f in request.FILES.getlist("file"):
+        path = os.path.join(upload_to or UPLOAD_PATH, f.name)
+        real_path = default_storage.save(path, f)
+        images = {'filelink': os.path.join(settings.MEDIA_URL, real_path)}
+        return HttpResponse(json.dumps(images), mimetype="application/json")
